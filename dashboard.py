7c2e06407d61cf9846780a225b7571e4ce32bbb0
YJH <47211830+YooJaeHong2@users.noreply.github.com>
2024-09-21 16:22:26 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -12,14 +12,6 @@ app.layout = html.Div([
     dcc.Input(id='my-input', value='초기값', type='text'),
     html.Div(id='my-output') ])
 
-@app.before_request
-def create_tables():
-    # The following line will remove this handler, making it
-    # only run on the first request
-    app.before_request_funcs[None].remove(create_tables)
-
-    db.create_all()
-
 # 콜백 정의
 @app.callback(
     Output(component_id='my-output', component_property='children'),
