73d3f3f4942724f7a5752395066da4b42c501b6b
YJH <47211830+YooJaeHong2@users.noreply.github.com>
2024-09-21 16:08:23 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,45 +1,23 @@
 import dash
-from dash import html
-import pandas as pd
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
 
-# Pandas DataFrame 로드
-df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
-
-
-# HTML 테이블 요소 파싱
-def generate_table(dataframe, max_rows=10):
-    return html.Table([
-        html.Thead(
-            html.Tr([html.Th(col) for col in dataframe.columns])
-        ),
-        html.Tbody([
-            html.Tr([
-                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-            ]) for i in range(min(len(dataframe), max_rows))
-        ])
-    ])
-
-
-# Dash 앱 인스턴스 생성    
+# Dash 애플리케이션 초기화
 app = dash.Dash(__name__)
 server = app.server
 
-# In place of something like this
-@app.before_first_request
-def create_tables():
-    db.create_all()
-    ...
-
-# push context manually to app
-with app.app_context():
-    db.create_all()
-
-# 레이아웃 생성
+# 애플리케이션 레이아웃 설정
 app.layout = html.Div([
-    html.H4(children='US Agriculture Exports (2011)'),
-    generate_table(df)
-])
+    dcc.Input(id='my-input', value='초기값', type='text'),
+    html.Div(id='my-output') ])
+
+# 콜백 정의
+@app.callback(
+    Output(component_id='my-output', component_property='children'),
+    [Input(component_id='my-input', component_property='value')])
+def update_output_div(input_value):
+    return f'입력된 값: {input_value}'
 
 if __name__ == '__main__':
-    # Dash 앱 실행
-    app.run_server(debug=True)
+    app.run_server(debug=True)  # 서버 실행
